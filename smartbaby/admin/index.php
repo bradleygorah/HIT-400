62a8b80c4abaa8630aaf0dd6aeac73900b3591c7
bradleygorah <73293558+bradleygorah@users.noreply.github.com>
2021-08-09 02:49:27 +0200

    Create index.php

---
+++ smartbaby/admin/index.php
@@ -0,0 +1,71 @@
+<?php include("header.php"); ?>
+<section style="background-color: black;">
+<div class="container-fluid p-0 text-center" style="height: 100px;">
+<div class="text-center">
+    <h1 class="text-light">Smart baby Monitor Admin Panel</h1>
+</div>
+</div>
+</section>
+<br><br>
+<section id="portfolio" class="p-0">
+            <div class="container-fluid p-0">
+                <div class="row row-cols-1 row-cols-md-2 g-4 mx-auto" style="width: 90%;">
+  <div class="col">
+  <br>
+    <div class="card">
+    <a href="register.php">
+      <div class="card-body">
+        <h5 class="card-title">Check Up Register</h5>
+        <p class="card-text">Manage medicine collection</p>
+      </div>
+    </a>
+    </div>
+  </div>  <div class="col">
+  <br>
+    <div class="card">
+    <a href="courses.php">
+      <div class="card-body">
+        <h5 class="card-title">Courses</h5>
+        <p class="card-text">Manage medical courses</p>
+      </div>
+      </a>
+    </div>
+  </div>
+  <div class="col">
+  <br>
+    <div class="card">
+    <a href="health.php">
+      <div class="card-body">
+        <h5 class="card-title">Health Tips</h5>
+        <p class="card-text">Manage Health tips</p>
+      </div>
+      </a>
+    </div>
+  </div>
+  <div class="col">
+  <br>
+    <div class="card">
+    <a href="users.php">
+      <div class="card-body">
+        <h5 class="card-title">Users</h5>
+        <p class="card-text">Manage users</p>
+      </div>
+    </a>
+    </div>
+  </div>
+ 
+  <div class="col">
+  <br>
+    <div class="card">
+    <a href="../index.php">
+      <div class="card-body">
+        <h5 class="card-title">Home</h5>
+        <p class="card-text">Return to the landing page</p>
+      </div>
+    </a>
+    </div>
+  </div>
+            </div>
+        </section>
+        <br><br>
+<?php include("footer.php"); ?>
